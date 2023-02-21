a3d4ff4ef81af3ededf0f841981cb97cec586df5
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-02-21 14:30:32 -0500

    Please help I don't understand queries... rest is done.

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,14 +7,14 @@ VALUES
 
 INSERT INTO role (title, salary, dept_id)
 VALUES
-    ("Sales Lead", 100000, 1),
-    ("Salesperson", 80000, 2),
-    ("Lead Engineer", 150000, 3),
-    ("Software Engineer", 120000, 4),
-    ("Account Manager", 160000, 5),
-    ("Accountant", 125000, 6),
-    ("Legal Team Lead", 250000, 7),
-    ("Lawyer", 190000, 8);
+    ("Sales Lead", 100000, 4),
+    ("Salesperson", 80000, 4),
+    ("Lead Engineer", 150000, 1),
+    ("Software Engineer", 120000, 1),
+    ("Account Manager", 160000, 2),
+    ("Accountant", 125000, 2),
+    ("Legal Team Lead", 250000, 3),
+    ("Lawyer", 190000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const appStart = () => {
                     "View All Employees",
                     "Add a Department",
                     "Add an Employee",
+                    "Add an Employee Role",
                     "Update Employee Role",
                 ],
             },
@@ -44,6 +45,9 @@ const appStart = () => {
                     break;
                 case "Add an Employee":
                     addEmp();
+                    break;
+                case "Add an Employee Role":
+                    addRole();
                     break;
                 case "Update Employee Role":
                     updateRole();
@@ -112,22 +116,22 @@ function addEmp() {
             {
                 name: "role",
                 type: "list",
-                message: "What is the employee's role?",
-                choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"],
+                message: "Role? 1-Sales Lead, 2-Salesperson, 3-Lead Engineer, 4-Software Engineer, 5-Account Manager, 6-Accountant, 7-Legal Team Lead, 8-Lawyer",
+                choices: [1, 2, 3, 4, 5, 6, 7, 8],
             },
             {
                 name: "manager",
                 type: "list",
-                message: "Who is the employee's manager?",
-                choices: ["John Doe", "Mike Chan", "Ashley Rodriguez", "Kevin Tupik", "Kunal Singh", "Malia Brown", "Sarah Lourd", "Tom Allen"],
+                message: "Manager? 1-John Doe, 2-Mike Chan, 3-Ashley Rodriguez, 4-Kevin Tupik, 5-Kunal Singh, 6-Malia Brown, 7-Sarah Lourd, 8-Tom Allen",
+                choices: [1, 2, 3, 4, 5, 6, 7, 8],
             },
         ])
         .then ((res) => {
             const newEmployee = {
                 first_name: res.first,
                 last_name: res.last,
-                manager_id: manager_id,
-                role_id: role_id,
+                role_id: res.role,
+                manager_id: res.manager,   
             };
 
             db.query("INSERT INTO employee SET ?", newEmployee, (err) => {
@@ -137,9 +141,40 @@ function addEmp() {
         });
 };
 
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                name: "newRole",
+                type: "input",
+                message: "What is the name of the Role you would like to add?",
+            },
+            {
+                name: "newSalary",
+                type: "input",
+                message: "What is the salary of this new role?",
+            },
+            {
+                name: "department",
+                type: "list",
+                message: "What department does this new role fall under? 1-Engineering, 2-Fincance, 3-Legal, 4-Sales",
+                choices: [1, 2, 3, 4],
+            },
+        ])
+        .then((res) => {
+            const query = {
+                title: res.newRole,
+                salary: res.newSalary,
+                dept_id: res.department,
+            };
+            db.query("INSERT INTO role SET ?", query, (err) => {
+                if (err) throw err;
+                appStart();
+            })
+        });
+};
+
 function updateRole() {
-
     
-
-}
+};
 appStart();
